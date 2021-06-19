58a3b4d7ea47ac5b474a6bfd0a78c7588c757d9c
Muslimbek <muslimerkinovich@gmail.com>
2021-06-19 20:20:29 +0500

    maket tugatildi

---
+++ script.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 6;
+console.log( a + b );
+
+console.log( a * b );
+let header = "Something in the header"
+
+let myName = "Muslimbek";
+let mySurname = "Erminjonov";
+
+let myYears = "21";
+console.log('Salom mening ismim ' + myName + ', familiyam ' + mySurname + '. Yoshim esa ' + myYears + "da");
